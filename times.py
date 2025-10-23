dbe589dc7a67f42c1d077725e216459ee01126ac
SamHolt <s.h.holt00@gmail.com>
2025-10-23 11:36:26 +0100

    Add test file with unit test for times.py

--- times.py
+++ times.py
@@ -15,7 +15,7 @@ def compute_overlap_time(range1, range2):
     overlap_time = []
     for start1, end1 in range1:
         for start2, end2 in range2:
-            low = max(start1, start2)
+            low = max(start1, start2) # This code should also check that low < high, but the test does not cover that case.
             high = min(end1, end2)
             overlap_time.append((low, high))
     return overlap_time
